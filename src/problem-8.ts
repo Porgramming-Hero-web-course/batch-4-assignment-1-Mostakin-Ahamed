6a00afb39e72c60ac140e041bde1c5c4459cb951
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-11-11 12:00:20 +0600

    added type of Person

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -5,7 +5,7 @@
     email: string;
   };
 
-  function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+  function validateKeys<T extends Person>(obj: T, keys: (keyof T)[]): boolean {
     for (const key of keys) {
       if (!(key in obj)) {
         return false;
